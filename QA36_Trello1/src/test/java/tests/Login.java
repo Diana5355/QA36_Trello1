c996dda8981a744393ce099124be8379211e1516
Diana5355 <dianasosina005@mail.ru>
2023-01-09 19:49:47 +0200

    Update Login.java

--- QA36_Trello1/src/test/java/tests/Login.java
+++ QA36_Trello1/src/test/java/tests/Login.java
@@ -1,67 +1,31 @@
 package tests;
 
-import org.testng.annotations.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-import java.time.Duration;
 
-
-public class Login {
-    WebDriver wd;
-
+public class Login extends TestBase {
 
     @BeforeMethod
     public void preConditions(){
-        wd = new ChromeDriver();
-        wd.manage().window().maximize();
-        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        wd.navigate().to("https://trello.com/");
+        if(app.getUser().isLogged()){
+            app.getUser().logOut();
+        }
     }
 
     @Test
     public void login1() {
-        click(By.cssSelector("[href='/login']"));
-        pause(2000);
-        type(By.cssSelector("#user"), "juliakliot.jk@gmail.com");
-        click(By.cssSelector("#login"));
-        pause(2000);
-        type(By.cssSelector("#password"), "misha240613");
-        click(By.cssSelector("#login-submit"));
-        pause(2000);
 
-        //[data-testid='header-member-menu-button']
-        //[data-testid='header-member-menu-logout']
-        //#logout-submit"
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillInLoginForm("juliakliot.jk@gmail.com", "misha240613");
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+
+        Assert.assertTrue(app.getUser().isLogged());
+
 
     }
 
-
-    private void type(By locator, String text) {
-        click(locator);
-        wd.findElement(By.cssSelector("#user")).clear();
-        wd.findElement(By.cssSelector("#user")).sendKeys(text);
-    }
-
-    public void click(By locator) {
-        wd.findElement(locator).click();
-    }
-
-
-    public void pause(int millis){
-        try{
-            Thread.sleep(millis);
-        }catch(InterruptedException e){
-            e.printStackTrace();
-        }
-    }
-
-    @AfterMethod
-    public void postConditions(){
-        wd.close();
-        wd.quit();
-    }
 }
